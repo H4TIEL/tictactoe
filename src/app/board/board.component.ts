3dfd47172297a255bf5a4836535c559c02cbc8ad
H4TIEL <github.com/H4TIEL>
2021-07-19 11:13:59 +0300

    Add minimax algorithm

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -11,17 +11,16 @@ export type Player = 'X' | 'O' | null;
 })
 export class BoardComponent implements OnInit {
 
-  squares: any[] = [];
-  public currentPlayer: Player = null;
-  winner: Player = null;
+  private human: 'X' = 'X';
+  private agent: 'O' = 'O';
+  private currentPlayer: Player = null;
+  private winner: Player = null;
 
-  human: 'X' = 'X';
-  agent: 'O' = 'O';
-  bestPlay: number = -Infinity;
+  public squares: any[] = [];
 
   constructor(public dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.newGame();
   }
 
@@ -33,46 +32,37 @@ export class BoardComponent implements OnInit {
     this.squares = Array(9).fill(null);
     this.currentPlayer = this.human;
     this.winner = null;
-
   }
-
 
   public makeMove(index: number): void {
     // human moves
-    this.humanMove(index);
+    this.move(index, this.human);
     this.currentPlayer = this.agent;
 
     // agent moves
     if (this.winner === null) {
-      this.agentMove();
+      // calculate move
+      let board: any[] = this.squares.slice();
+      let move = this.minimax(board, this.agent);
+      this.move(move, this.agent);
       this.currentPlayer = this.human;
     }
-
-
-    // minimax
-    let play = this.minimax(this.currentPlayer);
-
-
   }
 
-  private humanMove(index: number) {
+
+  private move(index: number, player: Player) {
     if (!this.squares[index]) {
-      this.squares.splice(index, 1, this.human);
+      this.squares.splice(index, 1, player);
     }
-    this.checkWinner(this.human);
+    if (this.checkWinner(this.squares, player)) {
+      this.winner = player;
+      this.openDialog(player);
+    }
   }
 
-  private agentMove() {
-    let allPossibleMoves = this.allPossibleMoves();
-    let move = allPossibleMoves[Math.floor(Math.random() * allPossibleMoves.length)];
-    this.squares.splice(move, 1, this.agent);
-    this.checkWinner(this.agent);
-  }
-
-
-  private allPossibleMoves(): any[] {
+  private allPossibleMoves(squares: any[]): any[] {
     let moves: any[] = [];
-    this.squares.forEach((square, index) => {
+    squares.forEach((square, index) => {
       if (square === null)
         moves.push(index);
     })
@@ -80,29 +70,79 @@ export class BoardComponent implements OnInit {
   }
 
 
-
-  private checkWinner(player: Player) {
-    if (
-      (this.squares[0] === player && this.squares[1] === player && this.squares[2] === player) ||
-      (this.squares[3] === player && this.squares[4] === player && this.squares[5] === player) ||
-      (this.squares[6] === player && this.squares[7] === player && this.squares[8] === player) ||
-      (this.squares[0] === player && this.squares[3] === player && this.squares[6] === player) ||
-      (this.squares[1] === player && this.squares[4] === player && this.squares[7] === player) ||
-      (this.squares[2] === player && this.squares[5] === player && this.squares[8] === player) ||
-      (this.squares[0] === player && this.squares[4] === player && this.squares[8] === player) ||
-      (this.squares[2] === player && this.squares[4] === player && this.squares[6] === player)
-    ) {
-      this.winner = player;
-      this.openDialog(this.winner);
-    } else {
-      if (this.squares.every(square => square !== null)) this.openDialog(null);
-    }
-
+  private allPossibleSquares(squares: any[], player: Player): any[][] {
+    let possibleSquares: any[][] = [];
+    let moves: any[] = this.allPossibleMoves(squares);
+    moves.forEach(move => {
+      let possible: any[] = squares.slice();
+      possible[move] = player;
+      possibleSquares.push(possible);
+    })
+    return possibleSquares;
   }
 
-  private minimax(player: Player) {
 
-    return null;
+  private checkWinner(squares: any[], player: Player): boolean | null {
+    if (
+      (squares[0] === player && squares[1] === player && squares[2] === player) ||
+      (squares[3] === player && squares[4] === player && squares[5] === player) ||
+      (squares[6] === player && squares[7] === player && squares[8] === player) ||
+      (squares[0] === player && squares[3] === player && squares[6] === player) ||
+      (squares[1] === player && squares[4] === player && squares[7] === player) ||
+      (squares[2] === player && squares[5] === player && squares[8] === player) ||
+      (squares[0] === player && squares[4] === player && squares[8] === player) ||
+      (squares[2] === player && squares[4] === player && squares[6] === player)
+    ) {
+      // player won
+      return true;
+    } else if (this.squares.every(square => square !== null)) {
+      // draw
+      return null;
+    }
+    else {
+      // no winner
+      return false;
+    }
+  }
+
+
+  private heuristicEvaluate(result: boolean | null, player: Player): number {
+    let score: number = 0;
+    if (result && player === this.agent)
+      score = 1;
+    else if (result && player === this.human)
+      score = -1;
+    return score;
+  }
+
+  /**
+   * 
+   * @param squares possible square moves
+   * @param player current player
+   * @returns move
+   */
+  private minimax(squares: any[], player: Player): number {
+    // game is complete
+    let result = this.checkWinner(squares, this.agent) || this.checkWinner(squares, this.human);
+    if (result || result === null)
+      return this.heuristicEvaluate(result, player);
+
+    if (player === this.agent) { // maximize
+      let maxScore = -Infinity;
+      let possibleSquares: any[][] = this.allPossibleSquares(squares, player);
+      possibleSquares.forEach(possibleSquare => {
+        maxScore = Math.max(maxScore, this.minimax(possibleSquare, this.human));
+      })
+      return maxScore;
+
+    } else { // minimize
+      let maxScore = +Infinity;
+      let possibleSquares: any[][] = this.allPossibleSquares(squares, player);
+      possibleSquares.forEach(possibleSquare => {
+        maxScore = Math.max(maxScore, this.minimax(possibleSquare, this.agent));
+      })
+      return maxScore;
+    }
   }
 
 
